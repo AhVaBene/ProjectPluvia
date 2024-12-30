cd9b1405cf3f96d8610b15f57abf2f0028b34a9b
Riccardo Alni <riccardo.alni@studio.unibo.it>
2024-12-30 17:07:17 +0100

    find user query

--- backend/src/controllers/userController.ts
+++ backend/src/controllers/userController.ts
@@ -2,5 +2,16 @@ import { Location } from "../../location";
 import { reportModel } from '../models/reportsModel';
 
 export class userController {
-    
+    static getUserbyId = (req, res) => {
+        reportModel.findById(req.params.id)
+        .then(doc => {
+            if (!doc) {
+                return res.status(404).send('User not found');
+            }
+            res.json(doc);
+        })
+        .catch(err => {
+            res.status(500).send(err);
+        });
+    }
 }

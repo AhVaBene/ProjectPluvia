79d9ec1e29bd431e385bc6e02d212cfa11543aec
Marco Pesic <marco.pesic@studio.unibo.it>
2025-01-02 14:12:30 +0100

    Added some basic controller functions for reports and routes

--- backend/src/controllers/reportController.ts
+++ backend/src/controllers/reportController.ts
@@ -19,4 +19,72 @@ export class reportController {
                 res.status(500).send(err);
             });
     }
+
+    static getAllReports = (res) =>{
+        reportModel.find()
+        .then(docs=>{
+            res.json(docs)
+        })
+        .catch(err=>{
+            res.satus(500).send(err)
+        })
+    }
+
+    static getOnlyVerifiedReports = (res) =>{
+        reportModel.find()
+        .where('riskLevel').gte(1)
+        .then(docs=>{
+            res.json(docs);
+        })
+        .catch(err=>{
+            res.status(500).send(err)
+        })
+    }
+
+    static updateReport = (req, res) => {
+        const { id } = req.params.id; // Get the report ID from the request parameters
+        const updateData = req.body; // Get the updated data from the request body
+    
+        reportModel
+          .findOneAndUpdate({ id }, updateData, { new: true }) // { new: true } returns the updated document
+          .then((updatedReport) => {
+            if (!updatedReport) {
+              return res.status(404).send({ message: 'Report not found' });
+            }
+            res.json(updatedReport);
+          })
+          .catch((err) => {
+            res.status(500).send(err);
+          });
+      };
+
+      static deleteReport = (req, res) => {
+        reportModel.findByIdAndDelete(req.params.id)
+            .then(doc => {
+                if (!doc) {
+                    return res.status(404).send('report not found');
+                }
+                res.json({ message: 'report deleted' });
+            })
+            .catch(err => {
+                res.status(500).send(err);
+            });
+        }
+
+      static createReport = (req, res) => {
+        const reportData = req.body; // Get the report data from the request body
+        console.log(req.body)
+
+        const newReport = new reportModel(reportData);
+    
+        // newReport
+        //   .save()
+        //   .then((savedReport) => {
+        //     res.status(201).json(savedReport); // Respond with the created report and 201 status
+        //   })
+        //   .catch((err) => {
+        //     res.status(500).send(err);
+        //   });
+      };
+    
 }
--- backend/src/routes/reportRoutes.ts
+++ backend/src/routes/reportRoutes.ts
@@ -2,7 +2,13 @@ import express from 'express';
 const router = express.Router();
 import { reportController } from '../controllers/reportController';
 
+//router.route('/').get(reportController.getReportsNearby);
+
 router.route('/')
-    .get(reportController.getReportsNearby);
+    .get(reportController.getAllReports)
+    .post(reportController.createReport)
+router.route('/:id')
+    .put(reportController.updateReport)
+    .delete(reportController.deleteReport)
 
 export const reportRouter = router;
